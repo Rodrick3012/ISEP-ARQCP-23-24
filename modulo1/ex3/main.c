1d7e72d565e0478d7d5a4e245ad98c374d81eb51
RodrigoCardoso <1221083@isep.ipp.pt>
2023-10-04 17:41:59 +0100

    EX3 and EX5 realiztion Part 2

---
+++ modulo1/ex3/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void capitalize(char* str) {
+    if (str == NULL) {
+        return;
+    }
+    int i;
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] >= 'a' && str[i] <= 'z') {
+            str[i] = str[i] - ('a' - 'A');
+        }
+    }
+}
+
+int main() {
+    char myString[] = "Hello, World!";
+    printf("%s\n", myString);
+
+    capitalize(myString);
+
+    printf("%s\n", myString);
+
+    return 0;
+}
+
