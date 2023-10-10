249ede6f729747751b0955579dd607356c49246c
RodrigoCardoso <1221083@isep.ipp.pt>
2023-10-10 13:30:06 +0100

    EX7 Module Creation Part 2

--- modulo1/ex7/main.c
+++ modulo1/ex7/main.c
@@ -1,21 +1,5 @@
 #include <stdio.h>
-
-void get_array_statistics(int *vec, int n, int *min, int *max,float *avg){
-	*min = *vec;
-	*max = *vec;
-	int i;
-	int sum = 0;
-	for(i = 0; i < n; i++){
-		sum += *(vec+i);
-		if(*(vec+i) < *min){
-			*min = *(vec+i);
-		}
-		if(*(vec+i) > *max){
-			*max = *(vec+i);
-		}
-	}
-	*avg = (float) sum / n;
-}
+#include "get_array_statistics.h"
 
 int main() {
 	int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
