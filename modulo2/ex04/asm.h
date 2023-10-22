e2fd1854a3cbffa6c6c911a6f7e26b57fd2d3626
Mario_Joao <1221019@isep.ipp.pt>
2023-10-22 21:01:49 +0100

    ex6

--- modulo2/ex04/asm.h
+++ modulo2/ex04/asm.h
@@ -1,6 +1,5 @@
 #ifndef ASM_H 
 #define ASM_H 
-int sum(void);
 extern int64_t op3;
 extern int64_t op4;
 long yet_another_sum();
---
+++ modulo2/ex06/asm.s
@@ -0,0 +1,20 @@
+.section .data
+
+	
+	.global op1
+	.global op2
+
+	
+	
+.section .text
+	.global exchangeBytes # short exchangeBytes()
+	exchangeBytes:
+		movw %op1(%rip), %ax #place op1 in ax
+		movw %op2(%rip), %cx	#place op2 in cx
+		movb %ch, %cl	#most significant byte of op2 place in least signifiucant byte of op2
+		addb %ah, %ah  #most significant byte of op2 multiplied by 2 and stored in the same place %ah
+		movb %ah, %ch 	#most significant byte of op1 is now most significant byte of op2
+		
+		
+		
+		ret
