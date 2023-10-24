b1a68796531d53163e745616e014232cc0f952fe
Rodrigo <1221083@isep.ipp.pt>
2023-10-24 13:47:13 +0100

    EX11 Comments Added

--- modulo2/ex11/asm.s
+++ modulo2/ex11/asm.s
@@ -6,9 +6,9 @@
 	verify_flags:
 		movw op1(%rip), %cx # sets variable op1 in cx register
 		addw op2(%rip), %cx # adds op1 to op2
-		jc carry_flag
-		jo overflow_flag
-		movb $0, %al
+		jc carry_flag # jump to function carry_flag if carry flag is activated
+		jo overflow_flag # jump to function overflow_flag is overflow flag is activated
+		movb $0, %al # if not one of the two flags are activated, returns 0
 		ret
 	carry_flag:
 		movb $1, %al # returns 1 if carry flag is activated
