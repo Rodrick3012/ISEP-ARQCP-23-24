a1d553f312516a97bb797dc65e9545b2393fa93d
Rodrigo <1221083@isep.ipp.pt>
2023-11-03 10:41:48 +0000

    EX2 Comments Added

--- modulo3/ex02/asm.s
+++ modulo3/ex02/asm.s
@@ -1,18 +1,19 @@
 .section .text
 	.global str_copy_roman
 	str_copy_roman:
-		movb (%rdi), %al
+		movb (%rdi), %al # set the pointer of the input string to register al
 	my_loop:
-		cmpb $0, %al
-		je end
-		cmpb $'u', %al
-		jne not_replace
-		movb $'v', %al
+		cmpb $0, %al # compares the string terminator with the register al
+		je end # jump to end if string terminates
+		cmpb $'u', %al # compares the value ascii 'u' with the char in the position
+		jne not_replace # jump if the value ascii 'u' is not equals the char in the position
+		movb $'v', %al # replace value 'u' with value 'v'
 	not_replace:
-		movb %al, (%rsi)
-		incq %rdi
-		incq %rsi
-		movb (%rdi), %al
-		jmp my_loop
+		movb %al, (%rsi) # copy the char value to the destination pointer in the second parameter
+		incq %rdi # increments first parameter - move to the next char position
+		incq %rsi # increments second parameter - move to the next chat positon
+		movb (%rdi), %al # set the next value in the first parameter to the register al
+		jmp my_loop # jumps the loop
 	end:
+		movb $0, (%rsi) # null-terminates the output string
 		ret
