9e27d1befb7870db3fcdfe28b0d66cd24200004a
Rodrigo <1221083@isep.ipp.pt>
2023-11-03 10:47:07 +0000

    EX4 Comments Added

--- modulo3/ex04/asm.s
+++ modulo3/ex04/asm.s
@@ -1,14 +1,14 @@
 .section .text
 	.global vec_add_three
 	vec_add_three:
-	movl %esi, %ecx
-	movl $0, %edx
+	movl %esi, %ecx # set the length of array in register ecx
+	movl $0, %edx # set the counter of elements in register edx
 	my_loop:
-		cmpl %edx, %ecx
-		je end
-		leaq (%rdi, %rdx, 2), %rax
-		addw $3, (%rax)
-		incl %edx
-		jmp my_loop
+		cmpl %edx, %ecx # compares the counter of elements to the length of array
+		je end # if are equal, jumps to end
+		leaq (%rdi, %rdx, 2), %rax # sets the position of array in index with the scale of the value (word) and save in the register rax
+		addw $3, (%rax) # adds 3 to the value of the array 
+		incl %edx # increments the counter
+		jmp my_loop # make the loop
 	end:
-		ret
+		ret 
